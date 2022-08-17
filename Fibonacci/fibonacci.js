b08fcb3b4b764da87db36c4112ebdf479e19ac9c
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-08-17 16:44:26 -0500

    Create Iteration fibonacci function

--- Fibonacci/fibonacci.js
+++ Fibonacci/fibonacci.js
@@ -1,1 +1,16 @@
 console.log("hello world")
+
+let arr = []
+
+let fibs = (n) => {
+    for(let i = 0; i<=n; i++){
+        if(i <= 1) {
+            arr.push(1)
+        } else {
+            arr.push(arr[i-2] + arr[i-1])
+        }
+    }
+    return arr;
+}
+
+console.log(fibs(20))
