5b278ee39cf0c97caa324224b221d57ecc409d2b
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-08-17 17:25:05 -0500

    Create sumRange function

--- InterviewRecursion/index.js
+++ InterviewRecursion/index.js
@@ -1,1 +1,12 @@
 console.log("hello world")
+
+//Problem 1: Write a function called sumRange. It will take a number and return the sum of all numbers from 1 up to the number passed in.
+
+const sumRange = (n) => {
+    if(n === 1){
+        return 1
+    }
+    return n + sumRange(n-1)
+}
+
+console.log(sumRange(6))
